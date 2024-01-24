711eaa9b8f13d28e52db2ce424765b5f5f5e7ab2
Romario Ferreira <romarioffranca@gmail.com>
2024-01-24 20:22:40 -0300

    Instaling packages/Create ng-If component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DiretivaNgIfComponent } from './diretiva-ng-if/diretiva-ng-if.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiretivaNgIfComponent
   ],
   imports: [
     BrowserModule,
