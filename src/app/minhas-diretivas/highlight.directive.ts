b04063c3fcf8daca85a3d377d0382ba5ce04bb80
Romario Ferreira <romarioffranca@gmail.com>
2024-03-01 22:19:43 -0300

    revert update on highlight-directive

--- src/app/minhas-diretivas/highlight.directive.ts
+++ src/app/minhas-diretivas/highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Renderer2, HostListener, Input, HostBinding} from '@angular/core';
+import { Directive, ElementRef, Renderer2, HostListener, Input} from '@angular/core';
 
 @Directive({
   selector: '[highlight]'
@@ -8,30 +8,27 @@ export class HighlightDirective {
  @Input() color: string ='';
 
   constructor(
-    //private _elementRef: ElementRef,
-    //private _renderer: Renderer2
+    private _elementRef: ElementRef,
+    private _renderer: Renderer2
     ) {
 
     }
 
     @HostListener('mouseenter') onMouseEnter(){
-      this.color = (this.color != '') ? this.color : 'red';
+      this.highlight(this.color || 'red');
     }
 
     @HostListener('mouseleave') onMouseLeave(){
-      this.color ='';
+      this.highlight('');
     }
 
-  //  private highlight(color: string){
-  //     this._renderer.setStyle(
-  //       this._elementRef.nativeElement,
-  //       'background-color',
-  //       color
-  //     );
-  //   }
-
-    @HostBinding('style.backgroundColor') get setColor (){
-      return this.color;
+   private highlight(color: string){
+      this._renderer.setStyle(
+        this._elementRef.nativeElement,
+        'background-color',
+        color
+      );
     }
+
 
 }
